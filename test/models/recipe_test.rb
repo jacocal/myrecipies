9fbe27d1925375764819fca1e5fa1c762da3ff0a
Jacobo Calderon <cacovolt@gmail.com>
2016-04-16 23:29:04 +0000

    Tests and restrictions

---
+++ test/models/recipe_test.rb
@@ -0,0 +1,59 @@
+require "test_helper"
+
+class RecipeTest < ActiveSupport::TestCase
+  
+  def setup
+    @recipe = Recipe.new(name: "chicken", summary: "best chicken ever", 
+              description: "Add it all, squash it and finally fry it") #@ makes it an instance variable instead of a local variable
+  end
+  
+  test "recipe should be valid" do
+    assert @recipe.valid?
+  end
+  
+  test "name should be valid" do
+    @recipe.name = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "name length too long" do
+    @recipe.name = "a" * 101
+    assert_not @recipe.valid?
+  end
+  
+  test "name length too short" do
+    @recipe.name = "a" * 4
+    assert_not @recipe.valid?
+  end
+  
+  test "summary must be present" do
+    @recipe.summary = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "summary length too long" do
+    @recipe.summary = "a" * 151
+    assert_not @recipe.valid?
+  end
+  
+  test "summary length too short" do
+    @recipe.summary = "a" * 9
+    assert_not @recipe.valid?
+  end
+  
+  test "description must be present" do
+    @recipe.description = " "
+    assert_not @recipe.valid?
+  end
+  
+  test "description length too long" do
+    @recipe.description = "a" * 501
+    assert_not @recipe.valid?
+  end
+  
+  test "description length too short" do
+    @recipe.description = "a" * 19
+    assert_not @recipe.valid?
+  end
+  
+end

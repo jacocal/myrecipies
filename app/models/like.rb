0545e798980373e672157aa0af80f97d67e2bb40
Jacobo Calderon <cacovolt@gmail.com>
2016-04-22 13:01:45 +0000

    Added pagination

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,4 +1,5 @@
 class Like < ActiveRecord::Base
   belongs_to :chef
   belongs_to :recipe
+  validates_uniqueness_of :chef, scope: :recipe   #1 like for each recipe
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -7,6 +7,15 @@ class Recipe < ActiveRecord::Base
   validates :description, presence: true, length: { minimum: 20, maximum: 500 }
   mount_uploader :picture, PictureUploader
   validate :picture_size
+  default_scope -> { order(updated_at: :desc) }
+  
+  def thumbs_up_total
+    self.likes.where(like: true).size
+  end
+  
+  def thumbs_down_total
+  self.likes.where(like: false).size
+  end
   
   private
     def picture_size
